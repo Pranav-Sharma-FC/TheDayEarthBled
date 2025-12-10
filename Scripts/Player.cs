39251b9c563ee4c4513b4665756f2f31d0d5997f
Pranav-Sharma-FC <psharma@ftstudent.org>
2025-12-10 12:04:06 -0500

    Revert "Merge pull request #34 from Pranav-Sharma-FC/revert-33-FinishSprint1"
    This reverts commit 9f7577e968e18770ca78c160fd51863e509e1974, reversing
    changes made to ba8ddc05880800e50560b68fbcec48f9dc498e03.

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,15 +2,49 @@ using Godot;
 using Godot.Collections;
 using System;
 
+
+
 public partial class Player : Entity
 {
-	[Export] public int DeviceId = 0;
-	[Export] public bool player2; //Temporary bool
-	[Export] private CharacterBody2D enemy;
-	[Export] private RayCast2D rayCast;
-	public bool OutOfCameraRange;
+	//Enum
+	public enum PortFireMode
+	{
+		Single,
+		Burst,
+		Auto
+	}
+	public enum SternFireMode
+	{
+		Single,
+		Burst,
+		Auto
+	}
+	public enum StarboardFireMode
+	{
+		Single,
+		Burst,
+		Auto
+	}
+	[Export] private CharacterBody2D _enemy;
+	[Export] private Node2D _enemies;
+	[Export] private RayCast2D _rayCast;
+	[Export] private PackedScene _bullets;
+	[Export] private Node2D _bulletSpawn;
+	[Export] private Node2D _bulletTree;
+	[Export] private Node2D _portBullets;
+	[Export] private Node2D _sternBullets;
+	[Export] private Node2D _starboardBullets;
+	[Export] private Node2D _portAimers;
+	[Export] private Node2D _sternAimers;
+	[Export] private Node2D _starboardAimers;
+	private Node2D _currentAimer;
+	private bool _outOfCameraRange;
 	private Vector2 _velocity = Vector2.Zero;
 	private bool _activeThisFrame = true; // Only true if this player's device had input this frame
+	private bool _isBulletReloadTime = true;
+	private bool _isPortReloadTime = true;
+	private bool _isStarboardReloadTime = true;
+	private bool _isSternReloadTime = true;
 
 	// Called by GameManager
 	/*public override void _Input(InputEvent @event)
@@ -26,12 +60,16 @@ public partial class Player : Entity
 
 	public override void _PhysicsProcess(double delta)
 	{
+		if (isShield)
+		{
+			RegenSheilds();
+		}
 		AimWeapons();
 		if (Health <= 0)
 		{
 			Death();
 		}
-		if (!OutOfCameraRange)
+		if (!_outOfCameraRange)
 		{
 			//Only check Input.IsActionPressed if this player is active this frame
 			MoveCharacter(delta);
@@ -40,14 +78,22 @@ public partial class Player : Entity
 			Vector2 input = Vector2.Zero;
 			if (Input.IsActionPressed("thrust_left"))
 				input.X -= 1;
-			if (Input.IsActionPressed("thrust_right") && player2)
+			else if (Input.IsActionPressed("thrust_right"))
 				input.X += 1;
 			if (Input.IsActionPressed("thrust_up"))
 				input.Y -= 1;
-			if (Input.IsActionPressed("thrust_down"))
+			else if (Input.IsActionPressed("thrust_down"))
 				input.Y += 1;
+			if (Input.IsActionPressed("shoot"))
+				GD.Print(":laser");//Fire();
+			if (Input.IsActionPressed("shoot_port") & _isPortReloadTime)
+				FirePort();
+			if (Input.IsActionPressed("shoot_stern") & _isSternReloadTime)
+				FireStern();
+			if (Input.IsActionPressed("shoot_starboard") & _isStarboardReloadTime)
+				FireStarboard();
+			
 
-			GD.Print(Input.IsActionPressed("thrust_down"));
 			Velocity = Vector2.Zero;
 			input = input.Normalized();
 
@@ -58,12 +104,84 @@ public partial class Player : Entity
 
 			if (_velocity.Length() > _maxSpeed)
 				_velocity = _velocity.Normalized() * _maxSpeed;
+			
 
-			GD.Print(Velocity, _velocity);
 			Velocity = new Vector2(_velocity.X, _velocity.Y);
 			MoveAndSlide();
+			
+			_enemy = GetClosestPlayer();
+		}
+		if(Velocity.Length() > 0.01f)
+		{
+			Rotation = (Velocity.Angle() + Mathf.Pi/2f);
 		}
 	}
+	
+	public CharacterBody2D GetClosestPlayer()
+	{
+		Vector2 fromPosition = this.GlobalPosition;
+		CharacterBody2D closest = null;
+		float closestDist = float.MaxValue;
+		if (_enemies.GetChildCount() == 0)
+			return null;
+		foreach (CharacterBody2D child in _enemies.GetChildren())
+		{
+			if (child is Enemy enemys)
+			{
+				float dist = fromPosition.DistanceTo(enemys.GlobalPosition);
+				if (dist < closestDist)
+				{
+					closestDist = dist;
+					closest = enemys;
+				}
+			}
+		}
+
+		return closest;
+	}
+
+	private async void FireStern()
+	{
+		_isSternReloadTime = false;
+		foreach (Node2D stern in _sternBullets.GetChildren())
+		{
+			_bulletSpawn = stern;
+			_currentAimer = _sternAimers;
+			Fire();
+			await ToSignal(GetTree().CreateTimer(0.05), "timeout");
+		}
+		await ToSignal(GetTree().CreateTimer(0.1), "timeout");
+		_isSternReloadTime = true;
+	}
+	
+	private async void FireStarboard()
+	{
+		_isStarboardReloadTime = false;
+		foreach (Node2D sb in _starboardBullets.GetChildren())
+		{
+			_bulletSpawn = sb;
+			_currentAimer = _starboardAimers;
+			Fire();
+			await ToSignal(GetTree().CreateTimer(0.15), "timeout");
+		}
+		await ToSignal(GetTree().CreateTimer(0.75), "timeout");
+		_isStarboardReloadTime = true;
+	}
+	
+	private async void FirePort()
+	{
+		_isPortReloadTime = false;
+		foreach (Node2D port in _portBullets.GetChildren())
+		{
+			_bulletSpawn = port;
+			_currentAimer = _portAimers;
+			Fire();
+			await ToSignal(GetTree().CreateTimer(0.15), "timeout");
+		}
+		await ToSignal(GetTree().CreateTimer(0.75), "timeout");
+		_isPortReloadTime = true;
+	}
+
 
 	protected override void MoveCharacter(double delta)
 	{
@@ -74,20 +192,30 @@ public partial class Player : Entity
 	}
 	public override void TakeDamage(int enemysDamage)
 	{
-		Health -= enemysDamage;
+		Sheilds -= enemysDamage;
+		if (Sheilds < 0)
+		{
+			Health += Sheilds;
+			GD.Print("Health Debug" + Health);
+			Sheilds = 0;
+		}
 	}
 	protected override void SpecialEffects()
-	{
-		
-	}
-	protected override void HealthReload()
 	{
 		
 	}
 
 	protected override void Fire()
 	{
-		
+		_isBulletReloadTime = false;
+		Bullet bull = _bullets.Instantiate<Bullet>();
+		Vector2 randomOffset = new Vector2(
+			GD.RandRange(-50, 50),
+			GD.RandRange(-50, 50)
+		);
+		bull.target = _currentAimer;
+		bull.Position = _bulletSpawn.GlobalPosition + randomOffset;
+		_bulletTree.AddChild(bull);
 	}
 
 	public override Dictionary GetStats()
@@ -118,11 +246,24 @@ public partial class Player : Entity
 
 	private void AimWeapons()
 	{
-		if (!GodotObject.IsInstanceValid(enemy))
-			return
-				;
-		Vector2 targetPos = enemy.GlobalPosition;
-		rayCast.LookAt(targetPos);
+		if (!GodotObject.IsInstanceValid(_enemy))
+		{
+			_rayCast.RotationDegrees = 180;
+			_enemy = GetClosestPlayer();
+		}
+		else
+		{
+			Vector2 targetPos = _enemy.GlobalPosition;
+			_rayCast.LookAt(targetPos);
+			if (_rayCast.IsColliding())
+			{
+				Node2D objectss = (Node2D) _rayCast.GetCollider();
+				if (objectss.GetParent() is Enemy enemy) 
+				{
+					GD.Print("eadadfadfd");
+					enemy.TakeDamage(Damage);
+				}
+			}
+		}
 	}
-
 }
