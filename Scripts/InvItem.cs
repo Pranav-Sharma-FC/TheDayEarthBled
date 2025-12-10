de3401e3c70d62f35831a7f9437c8f8ce436056a
Aerospace820 <156375508+Aerospace820@users.noreply.github.com>
2025-12-10 00:33:13 -0500

    Started Work on Inventory
    Oh Brother

---
+++ Scripts/InvItem.cs
@@ -0,0 +1,8 @@
+using Godot;
+
+[Tool]
+public partial class InvItem : Resource
+{
+	[Export] public Texture2D Icon { get; set; }
+	[Export] public string Name { get; set; } = "";
+}
---
+++ Scripts/InvSlot.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class InvSlot : Panel
+{
+	private Sprite2D _item;
+	private Label _amount;
+	
+	
+}
