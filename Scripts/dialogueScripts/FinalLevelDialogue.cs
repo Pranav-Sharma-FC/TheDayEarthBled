0ef1e4605b51af527b95a133cc23ef8ab97231ee
anoor-2 <anoor@ftstudent.org>
2025-12-11 12:36:27 -0500

    SCRIPT

--- Scripts/dialogueScripts/FinalLevelDialogue.cs
+++ Scripts/dialogueScripts/FinalLevelDialogue.cs
@@ -3,4 +3,39 @@ using System;
 
 public partial class FinalLevelDialogue : Label
 {
+    [Export] private Label _referencetolabel;
+
+    private async void dialogue2()
+    {
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Alyx: Earth comms are silent. Fleet strength below nine percent";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Harper: Transmit to all ships: Hold the line at Earth horizon";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Harper: We will not win today. We will buy time for others to live";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Singh: Ammo dry. Switching to kinetic runs. See you on the other side";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Alien: We will unmake your Sun and your named with it.";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Harper: Eagle-One to all stations-fire everything. Now.";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Alyx: Tip-Ramming is the last resort. Save the carriers.";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Kodiak covers evac. phoenix protexts the Gate. Move!";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Alyx: Final objective: Hold for the evac timer, then retrest by wing";
+        await ToSignal(GetTree().CreateTimer(3), "timeout");
+        _referencetolabel.Text =
+            "Harper: UNSHC to all units...you did well. Earth bled, but never bowed.";
+    }
 }
--- Scripts/dialogueScripts/TutorialDialogue.cs
+++ Scripts/dialogueScripts/TutorialDialogue.cs
@@ -3,20 +3,23 @@ using System;
 
 public partial class TutorialDialogue : Label
 {
-    [Export] private Label _referenceToLabel;
+    public override void _Ready()
+    {
+        dialouge();
+    }
     private async void dialouge()
     {
         await ToSignal(GetTree().CreateTimer(3), "timeout");
-        _referenceToLabel.Text = 
+        this.Text = 
             "Alyx-Protocol: Admiral, use your right joystick to cycle fleet views. Left stick to adjust formation bearing. Camera lock to sector Tango-Four";
         await ToSignal(GetTree().CreateTimer(3), "timeout");
-        _referenceToLabel.Text =
+        this.Text =
             "Rear Admiral Sign (PHOENIX-LEAD): Eagle-One, this is Phoenix. Our frigate screen is ready for rally. UNSF Roosevelt and Yamato report green lights on railgun capacitators.";
         await ToSignal(GetTree().CreateTimer(3), "timeout");
-        _referenceToLabel.Text =
+        this.Text =
             "Harper: Good. Signal 'Ready Room Bravo'. Weapons free on all hostiles. Confirm IFF before firing-aliens are spoofing from transponders.";
         await ToSignal(GetTree().CreateTimer(3), "timeout");
-        _referenceToLabel.Text =
+        this.Text =
             "[Combat Cue]: Contact left! bearing zero-seven-zero! Enermy corvettes breaking formation! Fox-3. Fox-3!";
     }
 }
